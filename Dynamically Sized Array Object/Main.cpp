eeab2741840af8ce933e52b0a47a33e2452386a6
Noscka <Adam882200@gmail.com>
2022-06-27 21:04:15 +0100

    Testing custom data types passed

--- Dynamically Sized Array Object/Main.cpp
+++ Dynamically Sized Array Object/Main.cpp
@@ -1,18 +1,42 @@
 #include <iostream>
 #include "DynamicArrayObjectFiles/DynamicArray.h"
 
+/// <summary>
+/// Class for testing custom data types
+/// </summary>
+class DATestData
+{
+public:
+    int NumForTest;
+
+    DATestData() {}
+
+    DATestData(int number)
+    {
+        NumForTest = number;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const DATestData& Data)
+    {
+        os << Data.NumForTest;
+        return os;
+    }
+};
+
 int main()
 {
-    DynamicArray<int> Array = DynamicArray<int>(); // Create object
+
+
+    DynamicArray<DATestData> Array = DynamicArray<DATestData>(); // Create object
 
     //char textArray[24] = "TestTextWhichMakesSense"; // Test text to allow for easy mass append
-    //int textArray[] = {1,5,2,6,1,2,7,8,4,2,35,1,25,15,6}; // Test text to allow for easy mass append
+    //int textArray[] = {1,5,2,6,1,2,7,8,4,2,35,1,25,15,6}; // Test numbers to allow for easy mass append
     
     //Array.ArrayAppend(textArray, 24);
 
     //Array.Remove(1);
-    Array.Append(44);
-    Array.Append(22);
+    Array.Append(DATestData(2));
+    Array.Append(DATestData(6));
 
     std::cout << Array[1] << std::endl;
 
